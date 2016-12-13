da28d2fd40e60356402be8884df10aab3badc976
Gaspar Fernandez <gaspar.fernandez@totaki.com>
2016-12-13 19:53:36 +0100

    Prevent replaying if mouse over Status bar and actions (pause, show all, replay) Copy to clipboard whole term Copy to clipboard on double click

--- dist/show-your-terms.js
+++ dist/show-your-terms.js
@@ -1,13 +1,14 @@
 (function() {
   this.ShowYourTerms = (function() {
-    function ShowYourTerms(container, replay, termsOptions) {
-      var defaultOptions, i, j, key, ref, total, val;
-      this.container = container;
+    function ShowYourTerms(container1, replay, termsOptions) {
+      var container, defaultOptions, i, j, key, ref, toolbarData, total, val;
+      this.container = container1;
       this.replay = replay != null ? replay : true;
       this.termsOptions = termsOptions != null ? termsOptions : {};
       defaultOptions = {
         single: false,
-        pauseOnClick: true
+        pauseOnClick: true,
+        copyOnDblClick: true
       };
       for (key in defaultOptions) {
         val = defaultOptions[key];
@@ -16,11 +17,13 @@
         }
       }
       if (!this.container.nodeType) {
-        this.container = document.querySelectorAll(this.container);
+        this.container = Array.prototype.slice.call(document.querySelectorAll(this.container));
       }
       this.content = [];
-      this.sstatus = [];
+      this.dynamic = [];
       this.outputIndex = [];
+      this.termOptions = [];
+      toolbarData = '<div class="syt-statusbar"><div class="status"></div><ul class="tools"><li data-action="anim">Re-play</li><li data-action="copy">Copy to Clipboard</li><li data-action="pause">Pause</li><li data-action="full">Full View</li></ul></div>';
       if (this.container.length > 0) {
         total = this.container.length - 1;
         if (this.termsOptions['single']) {
@@ -28,8 +31,21 @@
         }
         for (i = j = 0, ref = total; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
           this.content[i] = [];
-          this.sstatus[i] = "ready";
+          this.dynamic[i] = {
+            status: "ready",
+            timers: {}
+          };
+          this.termOptions[i] = {
+            animate: !this.container[i].classList.contains('noanimate'),
+            statusbar: !this.container[i].classList.contains('nostatusbar')
+          };
+          this.container[i].innerHTML = '<div>' + this.container[i].innerHTML + '</div>';
+          if (this.termOptions[i].statusbar) {
+            this.container[i].innerHTML += toolbarData;
+          }
           this.defineEvents(i);
+          container = this.container[i];
+          this.container[i] = this.container[i].childNodes[0];
           if (this.container[i].innerText.length > 0) {
             this.declarativeBuilder(i);
           }
@@ -51,16 +67,131 @@
         ]);
       }
       this.container[outputTerm].style.minHeight = window.getComputedStyle(this.container[outputTerm], null).getPropertyValue("height");
-      return this.play(outputTerm);
+      if (this.termOptions[outputTerm].animate) {
+        return this.play(outputTerm);
+      } else {
+        return this.fullview(outputTerm);
+      }
+    };
+
+    ShowYourTerms.prototype.copyToClipboard = function(outputTerm, line) {
+      var content, el, err, error, j, lastType, len, options, ref, ref1, ref2, st, style, successful, tar, text, type;
+      if (line == null) {
+        line = -1;
+      }
+      try {
+        tar = document.createElement("textarea");
+        document.body.append(tar);
+        style = {
+          top: '-2em',
+          left: '-2em',
+          position: 'fixed',
+          background: 'transparent',
+          border: 'none',
+          outline: 'none',
+          boxShadow: 'none',
+          padding: '0',
+          overflow: 'hidden',
+          width: '2em',
+          height: '2em'
+        };
+        for (st in style) {
+          tar.style[st] = style[st];
+        }
+        text = '';
+        if (line === -1) {
+          ref = this.content[outputTerm];
+          for (j = 0, len = ref.length; j < len; j++) {
+            el = ref[j];
+            type = el[0], content = el[1], options = el[2];
+            text += content + "\n";
+          }
+        } else {
+          ref1 = this.content[outputTerm][line++], type = ref1[0], content = ref1[1], options = ref1[2];
+          lastType = type;
+          text += content + "\n";
+          while (line < this.content[outputTerm].length) {
+            ref2 = this.content[outputTerm][line++], type = ref2[0], content = ref2[1], options = ref2[2];
+            if (type !== lastType || type !== 'line') {
+              break;
+            }
+            lastType = type;
+            text += content + "\n";
+          }
+        }
+        tar.value = text;
+        tar.select();
+        successful = document.execCommand('copy');
+        if (!successful) {
+          return alert("There was a problem copying to clipboard");
+        }
+      } catch (error) {
+        err = error;
+        return alert("Sorry, your browser does not support it");
+      }
     };
 
     ShowYourTerms.prototype.defineEvents = function(outputTerm) {
+      var j, len, results, toolbarElement, toolbarElements;
       if (this.termsOptions['pauseOnClick']) {
-        return this.container[outputTerm].addEventListener('click', ((function(_this) {
+        this.container[outputTerm].addEventListener('click', ((function(_this) {
           return function() {
             return _this.togglePause(outputTerm);
           };
         })(this)));
+      }
+      if (this.termsOptions['copyOnDblClick']) {
+        this.container[outputTerm].addEventListener('dblclick', ((function(_this) {
+          return function(event) {
+            var ele, line;
+            ele = event.target;
+            line = ele.getAttribute('data-line');
+            return _this.copyToClipboard(outputTerm, line);
+          };
+        })(this)), false);
+      }
+      this.container[outputTerm].addEventListener('mouseenter', ((function(_this) {
+        return function() {
+          _this.dynamic[outputTerm].mouseover = true;
+          if (_this.dynamic[outputTerm].timers.leaveReplay) {
+            return clearInterval(_this.dynamic[outputTerm].timers.leaveReplay);
+          }
+        };
+      })(this)), false);
+      this.container[outputTerm].addEventListener('mouseleave', ((function(_this) {
+        return function() {
+          _this.dynamic[outputTerm].mouseover = false;
+          if (_this.dynamic[outputTerm].leaveReplay) {
+            _this.dynamic[outputTerm].leaveReplay = false;
+            return _this.dynamic[outputTerm].timers.leaveReplay = setTimeout((function() {
+              return _this.playagain(outputTerm);
+            }), 1000);
+          }
+        };
+      })(this)), false);
+      if (this.termOptions[outputTerm].statusbar) {
+        toolbarElements = this.container[outputTerm].querySelectorAll('.syt-statusbar .tools li');
+        results = [];
+        for (j = 0, len = toolbarElements.length; j < len; j++) {
+          toolbarElement = toolbarElements[j];
+          results.push(toolbarElement.addEventListener('click', ((function(_this) {
+            return function(event) {
+              var target;
+              event.stopPropagation();
+              target = event.target.getAttribute('data-action');
+              if (target === 'anim') {
+                return _this.playagain(outputTerm);
+              } else if (target === 'full') {
+                return _this.fullview(outputTerm);
+              } else if (target === 'pause') {
+                return _this.togglePause(outputTerm);
+              } else if (target === 'copy') {
+                return _this.copyToClipboard(outputTerm);
+              }
+            };
+          })(this))));
+        }
+        return results;
       }
     };
 
@@ -82,57 +213,112 @@
       }
     };
 
+    ShowYourTerms.prototype.clearTimers = function(outputTerm) {
+      var results, timer;
+      if (outputTerm == null) {
+        outputTerm = 0;
+      }
+      results = [];
+      for (timer in this.dynamic[outputTerm].timers) {
+        results.push(clearInterval(this.dynamic[outputTerm].timers[timer]));
+      }
+      return results;
+    };
+
     ShowYourTerms.prototype.play = function(outputTerm) {
       if (outputTerm == null) {
         outputTerm = 0;
       }
-      if (this.sstatus[outputTerm] === "ready") {
-        this.sstatus[outputTerm] = "playing";
+      if (this.dynamic[outputTerm].status === "ready") {
+        this.clearTimers(outputTerm);
+        this.dynamic[outputTerm].status = "playing";
         this.container[outputTerm].innerHTML = '';
         this.outputIndex[outputTerm] = 0;
         return this.outputGenerator(this.content[outputTerm][this.outputIndex[outputTerm]], outputTerm);
       }
     };
 
-    ShowYourTerms.prototype.playagain = function(outputTerm) {
+    ShowYourTerms.prototype.fullview = function(outputTerm) {
+      var content, count, currentLine, el, fullview, j, len, options, ref, type;
       if (outputTerm == null) {
         outputTerm = 0;
       }
-      this.sstatus[outputTerm] = "ready";
+      this.dynamic[outputTerm].status = "fullview";
+      fullview = document.createElement("div");
+      count = 0;
+      ref = this.content[outputTerm];
+      for (j = 0, len = ref.length; j < len; j++) {
+        el = ref[j];
+        type = el[0], content = el[1], options = el[2];
+        currentLine = document.createElement("div");
+        currentLine.setAttribute('data-line', count++);
+        if (options.styles) {
+          currentLine.setAttribute("class", options.styles);
+        }
+        if (type === "command") {
+          if (!currentLine.classList.contains('type')) {
+            currentLine.classList.add('command');
+          }
+        }
+        currentLine.append(content);
+        fullview.append(currentLine);
+      }
+      this.container[outputTerm].innerHTML = fullview.innerHTML;
+      return this.clearTimers(outputTerm);
+    };
+
+    ShowYourTerms.prototype.playagain = function(outputTerm, mouseCheck) {
+      if (outputTerm == null) {
+        outputTerm = 0;
+      }
+      if (mouseCheck == null) {
+        mouseCheck = false;
+      }
+      if (mouseCheck && this.dynamic[outputTerm].mouseover) {
+        this.dynamic[outputTerm].leaveReplay = true;
+        this.dynamic[outputTerm].status = "waiting";
+        return;
+      }
+      this.dynamic[outputTerm].status = "ready";
       return this.play(outputTerm);
     };
 
     ShowYourTerms.prototype.callNextOutput = function(delay, outputTerm) {
-      this.outputIndex[outputTerm] += 1;
-      if (this.content[outputTerm][this.outputIndex[outputTerm]]) {
-        return setTimeout(((function(_this) {
-          return function() {
-            return _this.outputGenerator(_this.content[outputTerm][_this.outputIndex[outputTerm]], outputTerm);
-          };
-        })(this)), delay);
-      } else if (this.replay) {
-        return setTimeout(((function(_this) {
-          return function() {
-            return _this.playagain(outputTerm);
-          };
-        })(this)), delay);
+      if (this.isPlaying(outputTerm)) {
+        this.outputIndex[outputTerm] += 1;
+        if (this.termsOptions.overrideDelay) {
+          delay = this.termsOptions.overrideDelay;
+        }
+        if (this.content[outputTerm][this.outputIndex[outputTerm]]) {
+          return this.dynamic[outputTerm].timers.output = setTimeout(((function(_this) {
+            return function() {
+              return _this.outputGenerator(_this.content[outputTerm][_this.outputIndex[outputTerm]], outputTerm);
+            };
+          })(this)), delay);
+        } else if (this.replay) {
+          return this.dynamic[outputTerm].timers.replay = setTimeout(((function(_this) {
+            return function() {
+              return _this.playagain(outputTerm, true);
+            };
+          })(this)), delay);
+        }
       }
     };
 
     ShowYourTerms.prototype.isPlaying = function(outputTerm) {
-      return this.sstatus[outputTerm] === "playing";
+      return this.dynamic[outputTerm].status === "playing";
     };
 
     ShowYourTerms.prototype.togglePause = function(outputTerm) {
-      if (this.sstatus[outputTerm] === "playing") {
-        return this.sstatus[outputTerm] = "pause";
-      } else if (this.sstatus[outputTerm] === "pause") {
-        return this.sstatus[outputTerm] = "playing";
+      if (this.dynamic[outputTerm].status === "playing") {
+        return this.dynamic[outputTerm].status = "pause";
+      } else if (this.dynamic[outputTerm].status === "pause") {
+        return this.dynamic[outputTerm].status = "playing";
       }
     };
 
     ShowYourTerms.prototype.outputGenerator = function(output, outputTerm) {
-      var content, counter, currentLine, interval, options, pauseInterval, speed, type;
+      var content, counter, currentLine, options, speed, type;
       type = output[0], content = output[1], options = output[2];
       currentLine = document.createElement("div");
       if (options.styles) {
@@ -143,13 +329,17 @@
       } else {
         speed = 100;
       }
+      if (this.termsOptions.overrideSpeed) {
+        speed = this.termsOptions.overrideSpeed;
+      }
       currentLine.classList.add('active');
+      currentLine.setAttribute('data-line', this.outputIndex[outputTerm]);
       if (type === "command") {
         if (!currentLine.classList.contains('type')) {
           currentLine.classList.add('command');
         }
         counter = 0;
-        return interval = setInterval(((function(_this) {
+        return this.dynamic[outputTerm].timers.command = setInterval(((function(_this) {
           return function() {
             if (_this.isPlaying(outputTerm)) {
               currentLine.appendChild(document.createTextNode(content[counter]));
@@ -158,17 +348,17 @@
               if (counter === content.length) {
                 currentLine.classList.remove('active');
                 _this.callNextOutput(options.delay, outputTerm);
-                return clearInterval(interval);
+                return clearInterval(_this.dynamic[outputTerm].timers.command);
               }
             }
           };
         })(this)), speed);
       } else {
         if (!this.isPlaying(outputTerm)) {
-          return pauseInterval = setInterval(((function(_this) {
+          return this.dynamic[outputTerm].timers.pause = setInterval(((function(_this) {
             return function() {
               if (_this.isPlaying(outputTerm)) {
-                clearInterval(pauseInterval);
+                clearInterval(_this.dynamic[outputTerm].timers.pause);
                 return _this.callNextOutput(options.delay, outputTerm);
               }
             };
--- src/show-your-terms.coffee
+++ src/show-your-terms.coffee
@@ -1,39 +1,152 @@
 class @ShowYourTerms
   constructor: (@container, @replay = true, @termsOptions={}) ->
-    defaultOptions = { single: false,
-    pauseOnClick: true }
+    defaultOptions = {
+      single: false,
+      pauseOnClick: true,
+      copyOnDblClick: true,
+      # overrideSpeed: 0,
+      # overrideDelay: 1,
+    }
     for key,val of defaultOptions
       if key not of @termsOptions
         @termsOptions[key] = val
 
     unless @container.nodeType
-      @container = document.querySelectorAll(@container)
+      @container = Array.prototype.slice.call(document.querySelectorAll(@container))
+    
     @content = []
-    @sstatus = []
+    @dynamic = []
     @outputIndex = []
+    @termOptions = []
+    toolbarData = '<div class="syt-statusbar"><div class="status"></div><ul class="tools"><li data-action="anim">Re-play</li><li data-action="copy">Copy to Clipboard</li><li data-action="pause">Pause</li><li data-action="full">Full View</li></ul></div>'
+    
     if @container.length > 0
       total = @container.length-1
       if @termsOptions['single']
         total=0
-      for i in [0..total] 
+
+      for i in [0..total]
+        
         @content[i] = []
-        @sstatus[i] = "ready"
+        @dynamic[i] = {
+          status: "ready",
+          timers: {} }
+        @termOptions[i] = {
+          animate: (!@container[i].classList.contains('noanimate')),
+          statusbar: (!@container[i].classList.contains('nostatusbar'))
+        }
+        @container[i].innerHTML='<div>'+@container[i].innerHTML+'</div>'
+        if @termOptions[i].statusbar
+          @container[i].innerHTML+=toolbarData;
+        # @dynamic[i].toolbar = @container[i].querySelectorAll('.syt-statusbar')
+          
         @defineEvents i
+        container = @container[i]
+        @container[i] = @container[i].childNodes[0]
+
         if @container[i].innerText.length > 0
-          @declarativeBuilder(i)
+          @declarativeBuilder(i)          
 
   declarativeBuilder: (outputTerm) ->
     for element in @container[outputTerm].children
       @content[outputTerm].push [element.getAttribute('data-action'), element.innerText, {styles: element.classList, delay: element.getAttribute('data-delay'), speed: element.getAttribute('data-speed')}]
     @container[outputTerm].style.minHeight = window.getComputedStyle(@container[outputTerm], null).getPropertyValue "height"
-    @play(outputTerm)
+    if @termOptions[outputTerm].animate
+      @play(outputTerm)
+    else
+      @fullview(outputTerm)
+
+  copyToClipboard: (outputTerm, line=-1) ->
+    try 
+      tar=document.createElement "textarea"
+      document.body.append(tar)
+      style={
+        top:'-2em',
+        left:'-2em',
+        position:'fixed',
+        background:'transparent',
+        border : 'none',
+        outline : 'none',
+        boxShadow : 'none',
+        padding:'0',
+        overflow:'hidden',
+        width:'2em',     # some browsers don't copy to clipboard if size is smaller
+        height:'2em'
+      }
+      for st of style
+        tar.style[st] = style[st];
+
+      text=''
+      if line==-1
+        for el in @content[outputTerm]
+          [type, content, options] = el;
+          text+=content+"\n";        
+      else
+        [type, content, options] = @content[outputTerm][line++]
+        lastType = type;
+        text+=content+"\n";        
+        while line < @content[outputTerm].length
+          [type, content, options] = @content[outputTerm][line++]
+          if type != lastType || type != 'line'
+            break;
+          lastType = type;
+          text+=content+"\n";        
+
+      tar.value=text
+
+      tar.select()
+      successful = document.execCommand('copy')
+      unless successful
+        alert "There was a problem copying to clipboard"
+
+      # document.body.removeChild(tar)
+    catch err
+      alert "Sorry, your browser does not support it"
 
   defineEvents: (outputTerm) ->
     if @termsOptions['pauseOnClick']
       @container[outputTerm].addEventListener('click', ( =>
         @togglePause outputTerm
       ))
+      
+    if @termsOptions['copyOnDblClick']
+      @container[outputTerm].addEventListener('dblclick', ((event) =>
+        ele = event.target
+        line = ele.getAttribute('data-line')
+      
+        @copyToClipboard outputTerm, line
+      ), false)
 
+    @container[outputTerm].addEventListener('mouseenter', ( =>
+      @dynamic[outputTerm].mouseover = true
+      if (@dynamic[outputTerm].timers.leaveReplay)
+        clearInterval @dynamic[outputTerm].timers.leaveReplay
+    ), false)
+    
+    @container[outputTerm].addEventListener('mouseleave', ( =>
+      @dynamic[outputTerm].mouseover = false
+      if @dynamic[outputTerm].leaveReplay
+        @dynamic[outputTerm].leaveReplay=false
+        @dynamic[outputTerm].timers.leaveReplay = setTimeout (=> @playagain outputTerm), 1000
+    ), false)
+    
+    if @termOptions[outputTerm].statusbar
+      toolbarElements = @container[outputTerm].querySelectorAll('.syt-statusbar .tools li')
+      for toolbarElement in toolbarElements
+        toolbarElement.addEventListener('click', ((event) =>
+          event.stopPropagation();
+          target = event.target.getAttribute ('data-action')
+          if target == 'anim'
+            @playagain outputTerm
+          else if target == 'full'
+            @fullview outputTerm
+          else if target == 'pause'
+            @togglePause outputTerm
+          else if target == 'copy'
+            @copyToClipboard outputTerm
+      ))
+      
+   
   addCommand: (content, options, outputTerm=0) ->
     if outputTerm < @content.length
       @content[outputTerm].push ["command", content, options]
@@ -42,45 +155,80 @@ class @ShowYourTerms
     if outputTerm < @content.length
       @content[outputTerm].push ["line", content, options]
 
+  clearTimers: (outputTerm=0) ->
+    for timer of @dynamic[outputTerm].timers
+      clearInterval(@dynamic[outputTerm].timers[timer])
+      
   play: (outputTerm=0) ->
-    if @sstatus[outputTerm] == "ready"
-      @sstatus[outputTerm] = "playing"
-      @container[outputTerm].innerHTML = ''
+    if @dynamic[outputTerm].status == "ready"
+      @clearTimers outputTerm
+      @dynamic[outputTerm].status = "playing"
+      @container[outputTerm].innerHTML=''
       @outputIndex[outputTerm] = 0
       @outputGenerator @content[outputTerm][@outputIndex[outputTerm]], outputTerm
 
-  playagain: (outputTerm=0) ->
-    @sstatus[outputTerm] = "ready"
+  fullview: (outputTerm=0) ->
+    @dynamic[outputTerm].status = "fullview"
+    fullview=document.createElement "div"
+    count=0
+    for el in @content[outputTerm]
+      [type, content, options] = el;
+      currentLine = document.createElement "div"
+      currentLine.setAttribute('data-line', count++)
+      if options.styles then currentLine.setAttribute "class", options.styles
+
+      if type == "command"
+        if not currentLine.classList.contains 'type'
+          currentLine.classList.add 'command'
+      currentLine.append(content)
+      fullview.append currentLine
+
+    @container[outputTerm].innerHTML=fullview.innerHTML;
+    @clearTimers outputTerm
+    
+  playagain: (outputTerm=0, mouseCheck=false) ->
+    if mouseCheck && @dynamic[outputTerm].mouseover 
+      @dynamic[outputTerm].leaveReplay=true
+      @dynamic[outputTerm].status = "waiting"
+      return
+      
+    @dynamic[outputTerm].status = "ready"
     @play outputTerm
 
   callNextOutput: (delay, outputTerm) ->
-    @outputIndex[outputTerm] += 1
-    if @content[outputTerm][@outputIndex[outputTerm]]
-      setTimeout (=> @outputGenerator @content[outputTerm][@outputIndex[outputTerm]], outputTerm), delay
-    else if @replay
-      setTimeout (=> @playagain(outputTerm)), delay
+    if @isPlaying outputTerm
+      @outputIndex[outputTerm] += 1
+      if @termsOptions.overrideDelay then delay = @termsOptions.overrideDelay
+
+      if @content[outputTerm][@outputIndex[outputTerm]]
+        @dynamic[outputTerm].timers.output = setTimeout (=> @outputGenerator @content[outputTerm][@outputIndex[outputTerm]], outputTerm), delay
+      else if @replay
+        @dynamic[outputTerm].timers.replay = setTimeout (=> @playagain(outputTerm, true)), delay
 
   isPlaying: (outputTerm) ->
-    return @sstatus[outputTerm] == "playing"
+    return @dynamic[outputTerm].status == "playing"
 
   togglePause: (outputTerm) ->
-    if @sstatus[outputTerm] == "playing"
-      @sstatus[outputTerm] = "pause";
-    else if @sstatus[outputTerm] == "pause"
-      @sstatus[outputTerm] = "playing"
+    if @dynamic[outputTerm].status == "playing"
+      @dynamic[outputTerm].status = "pause";
+    else if @dynamic[outputTerm].status == "pause"
+      @dynamic[outputTerm].status = "playing"
 
   outputGenerator: (output, outputTerm) ->
     [type, content, options] = output
     currentLine = document.createElement "div"
     if options.styles then currentLine.setAttribute "class", options.styles
     if options.speed then speed = options.speed else speed = 100
+    if @termsOptions.overrideSpeed then speed = @termsOptions.overrideSpeed
+
     currentLine.classList.add 'active'
+    currentLine.setAttribute('data-line', @outputIndex[outputTerm])
 
     if type == "command"
       if not currentLine.classList.contains 'type'
         currentLine.classList.add 'command'
       counter = 0
-      interval = setInterval ( =>
+      @dynamic[outputTerm].timers.command = setInterval ( =>
         if @isPlaying outputTerm
           currentLine.appendChild document.createTextNode(content[counter])
           @container[outputTerm].appendChild currentLine
@@ -88,13 +236,13 @@ class @ShowYourTerms
           if counter == content.length
             currentLine.classList.remove 'active'
             @callNextOutput options.delay,outputTerm
-            clearInterval interval
+            clearInterval @dynamic[outputTerm].timers.command
       ), speed
     else
       if not @isPlaying outputTerm
-        pauseInterval = setInterval ( =>
+        @dynamic[outputTerm].timers.pause = setInterval ( =>
           if @isPlaying outputTerm
-            clearInterval pauseInterval
+            clearInterval @dynamic[outputTerm].timers.pause
             @callNextOutput options.delay,outputTerm
 	), speed
       else
